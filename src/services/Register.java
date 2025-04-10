5537915a5637f03c0a6d0cab6ffb4e65d9334eb7
kunallad0007 <kunallad0007@gmail.com>
2025-04-10 13:28:00 +0530

    logic for password and rollno

--- src/services/Register.java
+++ src/services/Register.java
@@ -4,7 +4,10 @@ import utility.InputUility;
 
 public class Register{
 
-    
+    final String RED = "\u001B[31m";
+    final String RESET = "\u001B[0m";
+
+
     InputUility newinput = new InputUility();
     
     public StudentDetails getStudentDetails(){
@@ -18,14 +21,23 @@ public class Register{
         int studentAge = newinput.input.nextInt();
         newinput.input.nextLine();
 
-        System.out.println("Enter your DOB(DD-MM-YYYY): ");
+        System.out.print("Enter your DOB(DD-MM-YYYY): ");
         String studentDOB = newinput.input.nextLine();
 
         System.out.print("Enter your address: ");
         String studentAddress = newinput.input.nextLine();
 
-        System.out.print("Enter your roll number: ");
-        String studentRollNo = newinput.input.nextLine();
+        System.out.print("Enter 4 digit roll number: ");
+        String studentRollNo;
+        while(true){
+            System.out.print("Enter 4 digit roll number: ");
+            studentRollNo = newinput.input.nextLine();
+            if (studentRollNo.length() == 4) {
+                break;
+            } else {
+                System.err.println(RED+"*** Roll Number must be exactly 4 digits ***"+RESET);
+            }
+        }
 
         System.out.print("Enter your class: ");
         String studentStd = newinput.input.nextLine();
@@ -35,7 +47,15 @@ public class Register{
         newinput.input.nextLine();
 
         System.out.println("Enter 8 Digit Password: ");
-        String password = newinput.input.nextLine();
+        String password;
+        while(true){
+            password = newinput.input.nextLine();
+            if(password.length() == 8){
+                break;
+            }else{
+                System.err.print(RED+"*** Password must be exactly 8 digits ***"+RESET);
+            }
+        }
 
         newStudent.setStudentDetails(studentName, studentAge, studentDOB, studentAddress, studentRollNo, studentStd, studentDivision, password);
 
